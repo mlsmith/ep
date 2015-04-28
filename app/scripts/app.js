3e4918e54c1841deb029514823ac59a62dc8e7df
Julian Pittleman <jnpn87@gmail.com>
2015-04-28 04:36:34 -0400

    wire up to backend

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,9 +15,10 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'vr.directives.slider'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $httpProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -26,4 +27,7 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+
+    // $httpProvider.defaults.useXDomain = true;
+    // delete $httpProvider.defaults.headers.common['X-Requested-With'];
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,79 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name etherpokerApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the etherpokerApp
- */
 angular.module('etherpokerApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $timeout, $http, Game, Player, Socket) {
+    // var PokerEvaluator = require('poker-evaluator');
+  	// Get player ID from separate API (hardcoded for demo)
+  	var game;
+
+    Socket.on('gameStart', function (data) {
+      console.log(data);
+      game = $scope.game = data;
+      $scope.newWager = $scope.minBet = game.blinds[1] * 2;
+    });
+
+    
+
+    $scope.$watch('game.actionOn', function (actionOn) {
+      console.log(actionOn);
+    	if (actionOn === 0) {
+    		$scope.showActions = true;
+    	} else {
+    		$scope.showActions = false;
+    	}
+    });
+
+    $scope.$watch('game.currentWager', function (currentWager, prevWager) {
+    	if (currentWager) {
+    		prevWager = prevWager || 0;
+    		$scope.newWager = (currentWager * 2) - prevWager;
+    	}
+    });
+
+    $scope.cardImg = function (player, card) {
+      return '../images/cards/' + player.hand[card] + '.png';
+    }
+
+    $scope.fold = function () {
+    	Socket.emit('action', {
+    		type: 'fold',
+    		player: 0
+    	});
+    };
+    $scope.check = function () {
+    	Socket.emit('action', {
+    		type: 'check',
+    		player: 0
+    	});
+    };
+
+    $scope.call = function (amount) {
+    	Socket.emit('action', {
+    		type: 'call',
+    		player: 0,
+    		amount: amount
+    	});
+    };
+    $scope.bet = function (amount) {
+    	Socket.emit('action', {
+    		type: 'bet',
+    		player: 0,
+    		amount: amount
+    	});
+    };
+    $scope.raise = function (amount) {
+    	Socket.emit('action', {
+    		type: 'raise',
+    		player: 0,
+    		amount: amount
+    	});
+    };
+
+    function showHands () {
+    	$scope.showHands = true;
+    	$timeout(function(){
+    		$scope.showHands = false;
+    	}, 3000);
+    }
+
   });
